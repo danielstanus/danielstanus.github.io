eb44548305edee49689945e92d0351782ff912b7
danielstanus <daniel.stanus@edu.uah.es>
2025-03-08 20:09:15 +0100

    Corregir configuración para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '',
   images: {
     unoptimized: true,
   },
   // Asegura que el sitio funcione correctamente en GitHub Pages
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://danielstanus.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
+  trailingSlash: false,
 };
 
 export default nextConfig;
